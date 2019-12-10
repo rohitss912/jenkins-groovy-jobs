cdb6282c2160749cc747445c77492b75b6c1b090
rohitss912 <rohitss912@gmail.com>
2019-12-11 01:22:39 +0530

    updated pipeline groovy

--- Jobs/test_api_pipeline.groovy
+++ Jobs/test_api_pipeline.groovy
@@ -1,5 +1,9 @@
 node {
-    stage ('checkout') {
-            echo "Hi m there"
+    stage('Example') {
+        if (env.BRANCH_NAME == 'master') {
+            echo 'I only execute on the master branch'
+        } else {
+            echo 'I execute elsewhere'
+        }
     }
 }
