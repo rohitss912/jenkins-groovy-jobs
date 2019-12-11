6605b71fc2c26ccb48e0c51ffce0d7f4bb9be1ee
rohitss912 <rohitss912@gmail.com>
2019-12-12 00:18:44 +0530

    updated mvn build cmd

--- Jobs/test_api_pipeline.groovy
+++ Jobs/test_api_pipeline.groovy
@@ -3,4 +3,8 @@ node {
        checkout([$class: 'GitSCM', branches: [[name: '*/master']],
         userRemoteConfigs: [[url: 'https://github.com/rohitss912/springboot.git', credentialsId : 'credentialsId']]])
     }
+
+    stage('Build'){
+        sh "mvn clean package"
+    }
 }
